76f60f53bcdae525cdd43a6135b7bd28b465d01c
davybolivar <davy.bolivar@gmail.com>
2021-07-05 17:24:09 +0800

    chore: remove purge in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  purge: {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    options: {
-      safelist: [/(from|via|to|border|bg|text)-(.*)-(\\d{1}0{1,2})/]
-    },   
-  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
